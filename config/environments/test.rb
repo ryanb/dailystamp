2bae9cbfe95bbae3ff9b5106f6edecb994ef78c4
Ryan Bates <ryan@railscasts.com>
2009-08-21 17:45:59 -0700

    adding rspec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,3 +26,7 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+config.gem 'thoughtbot-factory_girl', :version => '>= 1.1.1', :lib => 'factory_girl', :source => 'http://gems.github.com'
+config.gem "rspec", :lib => false, :version => ">= 1.2.0"
+config.gem "rspec-rails", :lib => false, :version => ">= 1.2.0"
