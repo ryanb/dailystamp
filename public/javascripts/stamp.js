4ad466b964492100ede6238073ec899ca537dafa
Ryan Bates <ryan@railscasts.com>
2009-08-23 15:23:30 -0700

    only sending auth token if request is POST type, fixes problem with IE 7 on get ajax requests

--- public/javascripts/stamp.js
+++ public/javascripts/stamp.js
@@ -1,5 +1,5 @@
 $(document).ajaxSend(function(event, request, settings) {
-  if (typeof(AUTH_TOKEN) == "undefined") return;
+  if (settings.type == "GET" || typeof(AUTH_TOKEN) == "undefined") return;
   // settings.data is a serialized string like "foo=bar&baz=boink" (or null)
   settings.data = settings.data || "";
   settings.data += (settings.data ? "&" : "") + "authenticity_token=" + encodeURIComponent(AUTH_TOKEN);
