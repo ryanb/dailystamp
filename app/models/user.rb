8b8c186dd5faf2ad42627b659b46e3f54d3a789f
Ryan Bates <ryan@railscasts.com>
2009-09-05 09:37:06 -0700

    don't require username and email with openid - closes #8

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,25 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
-    c.openid_required_fields = [:nickname, :email]
+    c.openid_optional_fields = [:nickname, :email]
     c.merge_validates_confirmation_of_password_field_options :unless => :guest?
     c.merge_validates_length_of_password_field_options :unless => :guest?
     c.merge_validates_length_of_password_confirmation_field_options :unless => :guest?
-    c.merge_validates_length_of_login_field_options :unless => :guest
-    c.merge_validates_format_of_login_field_options :unless => :guest
-    c.merge_validates_uniqueness_of_login_field_options :unless => :guest
-    c.merge_validates_format_of_email_field_options :unless => :guest
-    c.merge_validates_length_of_email_field_options :unless => :guest
-    c.merge_validates_uniqueness_of_email_field_options :unless => :guest
+    c.merge_validates_length_of_login_field_options :unless => :guest_or_openid?
+    c.merge_validates_format_of_login_field_options :allow_blank => true
+    c.merge_validates_uniqueness_of_login_field_options :allow_blank => true
+    c.merge_validates_length_of_email_field_options :unless => :guest_or_openid?
+    c.merge_validates_format_of_email_field_options :allow_blank => true
+    c.merge_validates_uniqueness_of_email_field_options :allow_blank => true
   end
   
   has_many :stamps
   has_many :marks, :through => :stamps
   has_many :stamp_images
   belongs_to :current_stamp, :class_name => "Stamp"
+  
+  def guest_or_openid?
+    guest? || !openid_identifier.blank?
+  end
   
   def unused_color
     (STAMP_COLORS - stamps.map(&:color)).first
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,34 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe User do
+  it "should not have validations for guest user" do
+    User.new(:guest => true).should be_valid
+  end
+  
+  it "should not have username/email validations for openid user" do
+    User.new(:openid_identifier => "http://myopenid.com").should be_valid
+  end
+  
+  it "should validate email format for openid identifier" do
+    User.new(:openid_identifier => "http://myopenid.com", :email => "bad email").should have(1).error_on(:email)
+  end
+  
+  it "should validate username format for openid identifier" do
+    User.new(:openid_identifier => "http://myopenid.com", :username => "x").should have(1).error_on(:username)
+  end
+  
+  it "should require username when password is given" do
+    User.new(:password => "secret", :password_confirmation => "secret").should have(1).error_on(:username)
+  end
+  
+  it "should require username when password is given" do
+    User.new(:password => "secret", :password_confirmation => "secret").should_not be_valid
+  end
+  
+  it "should require username when password when nothing given" do
+    user = User.new
+    user.should_not be_valid
+    user.errors.on(:username).should =~ /too short/
+    user.errors.on(:password).should =~ /too short/
+  end
 end
