05bae1d33b938c741809702f72cb075cf411dbee
Ryan Bates <ryan@railscasts.com>
2009-09-11 20:13:19 -0700

    adding last seven stamp days to watched stamps section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,26 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def mark_image(mark, size = 70)
-    if mark
-      options = {:size => "#{size}x#{size}", :class => "mark"}
-      if mark.position_x && mark.position_y
-        options[:style] = "margin-left: #{mark.position_x-size/2-6}px; margin-top: #{mark.position_y-size/2-6}px;"
-      end
-      image_tag (mark.skip? ? "skip.png" : (mark.image_path || "question_mark.png")), options
+    options = {:size => "#{size}x#{size}", :class => "mark"}
+    if mark.position_x && mark.position_y
+      options[:style] = "margin-left: #{mark.position_x-size/2-6}px; margin-top: #{mark.position_y-size/2-6}px;"
+    end
+    image_tag mark_image_name(mark), options
+  end
+  
+  def small_mark_image(mark)
+    image_tag mark_image_name(mark), :size => "25x25"
+  end
+  
+  def mark_image_name(mark)
+    if mark.nil?
+      "spacer.gif"
+    elsif mark.skip?
+      "skip.png"
+    elsif mark.image_path.nil?
+      "question_mark.png"
+    else
+      mark.image_path
     end
   end
   
